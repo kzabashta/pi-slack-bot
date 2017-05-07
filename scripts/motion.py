add16bd693140f5a6ae3518abdbf40cfaa604824
Kosta Zabashta <kosta.zabashta@gmail.com>
2017-05-07 18:42:10 +0000

    Removing dropbox.. can upload to slack directly

--- scripts/motion.py
+++ scripts/motion.py
@@ -5,7 +5,6 @@ import json
 from datetime import datetime
 
 from slackclient import SlackClient
-import dropbox
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -19,28 +18,13 @@ slack_token = os.environ["SLACK_API_TOKEN"]
 client = SlackClient(slack_token)
 
 def upload_photo():
-       dbox_client = dropbox.client.DropboxClient(os.environ["DROPBOX_API_TOKEN"])
-       print os.environ["DROPBOX_API_TOKEN"]
-       fname = 'motion_%s.jpg' % datetime.now()
-       f = open('/var/lib/motion/lastsnap.jpg', 'rb')
-       response = dbox_client.put_file(fname, f)
-       link = dbox_client.share(fname)['url']
-       print(client.api_call('files.upload', channels="#general", filename='pic.jpg', file=open('/var/lib/motion/lastsnap.jpg', 'rb')))
+       client.api_call('files.upload', channels="#motion", filename='snapshot.jpg', title="Motion detected", file=open('/var/lib/motion/lastsnap.jpg', 'rb'))
        os.system('sudo rm -rf /var/lib/motion/*')
-       client.api_call(
-         "chat.postMessage",
-         channel="#general",
-         text="Motion Detected",
-           attachments=json.dumps([{"title": "Cat", "color": "#36a64f", "title_link": link, "thumb_url": link}])
-       )
        return link
 
 def save_photo():
        os.system('curl http://localhost:8080/0/action/snapshot')
        time.sleep(5)
-
-save_photo()
-upload_photo()
 
 while True:
        i=GPIO.input(23)
