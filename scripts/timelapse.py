d861403fad0587e242a9e957dfbee669bd7e854e
Kosta Zabashta <kosta.zabashta@gmail.com>
2017-07-08 01:50:27 +0000

    Switching to PiCamera

--- scripts/timelapse.py
+++ scripts/timelapse.py
@@ -1,8 +1,10 @@
 import time
 import os
 import json
-from datetime import date
+from datetime import date, timedelta
 import glob
+import shutil
+import picamera
 
 import imageio
 
@@ -10,20 +12,19 @@ FREQUENCY = 25
 IMAGES_DIR = 'timelapse/'
 
 
-def copy_photo(i):
+def save_photo(i):
     today_date = date.today().strftime('%Y_%m_%d')
     dir = os.path.join(IMAGES_DIR, today_date)
     try:
         os.stat(dir)
     except:
         os.mkdir(dir)
-    os.system('cp /var/lib/motion/lastsnap.jpg %s' %
-              os.path.join(dir, '%i.jpg' % i))
+    with picamera.PiCamera() as camera:
+        camera.resolution = (1024, 768)
+        camera.start_preview()
+        time.sleep(2)
+        camera.capture(os.path.join(dir, '%i.jpg' % i))
 
-
-def save_photo():
-    os.system('curl http://localhost:8080/0/action/snapshot')
-    time.sleep(5)
 
 def rename_files(dir):
     filepath_list = glob.glob(os.path.join(dir, '*.jpg'))
@@ -33,27 +34,34 @@ def rename_files(dir):
         os.rename(
             filepath,
             os.path.join(dir, 'pic_{:>0{}}.jpg'.format(n, pad))
-        )    
+        )
+
 
 def make_timelapse(today_date):
     images = []
     dir = os.path.join(IMAGES_DIR, today_date)
     rename_files(dir)
-    cmd = 'avconv -y -i ' + dir + '/pic_%4d.jpg -r 25 -vcodec libx264 -q:v 3 ' + '%s.mp4;' % today_date
+    cmd = 'avconv -y -i ' + dir + \
+        '/pic_%4d.jpg -r 25 -vcodec libx264 -q:v 3 ' + '%s.mp4;' % today_date
     print(cmd)
     os.system(cmd)
 
-def snap(i):
-    save_photo()
-    copy_photo(i)
 
+def snap(i):
+    save_photo(i)
+
+def remove_photos():
+    d = date.today() - timedelta(days=2).strftime('%Y_%m_%d')
+    shutil.rmtree(os.path.join(IMAGES_DIR, d))
 
 if __name__ == '__main__':
     today_date = date.today().strftime('%Y_%m_%d')
+    
     i = 0
     while True:
         if not date.today().strftime('%Y_%m_%d') == today_date:
             make_timelapse(today_date)
+            remove_photos()
             today_date = date.today().strftime('%Y_%m_%d')
             i = 0
         else:
